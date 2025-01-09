1bb98afd47504c5566db11feaec4cc2a8ab3e329
vlad-crab <vladikazakov05@gmail.com>
2025-01-09 22:21:47 +0500

    added weather now

--- handlers/weather.py
+++ handlers/weather.py
@@ -3,17 +3,20 @@ from aiogram.types import Message, KeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 from handlers.text_commands import commands
+from weather_requests.weather import weather
 
 router = Router()
 
 
 @router.message(F.location)
 async def send_weather_in_location(message: Message):
-    await message.answer(str(message.location.latitude) + "  |  " + str(message.location.longitude))
+    latitude, longitude = message.location.latitude, message.location.longitude
+    forecast = await weather.get_weather_forecast(latitude, longitude, number_of_days=1, weather_now=True)
+    await message.answer(text=forecast)
     await Message.delete(message)
 
 @router.callback_query(F.data == commands.request_location)
 async def request_location(callback: types.CallbackQuery):
     builder = ReplyKeyboardBuilder()
-    builder.row(KeyboardButton(text='Отправить локацию', request_location=True))
-    await callback.message.answer("дайти погоду пжлста", reply_markup=builder.as_markup(one_time_keyboard=True, resize_keyboard=True))
+    builder.row(KeyboardButton(text='Отправить текущую геопозицию', request_location=True))
+    await callback.message.answer("Отправьте геопозицию", reply_markup=builder.as_markup(one_time_keyboard=True, resize_keyboard=True))
